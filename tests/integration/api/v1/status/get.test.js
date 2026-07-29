8456035fa3b1ef3c3c6b388f845ca5d344ff1825
Fernando Braga <oferbraga108@gmail.com>
2026-07-29 14:36:42 -0300

    feat: add módulo de database ao projeto e variáveis de ambiente ao client do PG.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,8 @@
+import database from '../../../../../infra/database';
+
 test ('GET to /status should return status 200', async () => {
   const res = await fetch('http://localhost:3000/api/v1/status');
+  console.log(database);
   const data = await res.json();
   expect(data).toEqual({ status: 'ok', dbStatus: 'ok' });
 });
