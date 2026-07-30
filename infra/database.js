3da2f4e7961683668550743e2338308183509f58
Fernando Braga <oferbraga108@gmail.com>
2026-07-30 11:52:38 -0300

    refactor: altera database js.

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import {Client} from 'pg';
 
-async function query (sql, params) {
+async function query (queryObject) {
     const client = new Client({
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
@@ -9,7 +9,7 @@ async function query (sql, params) {
         database: process.env.POSTGRES_DB,
     });
     await client.connect();
-    const response = await client.query(sql, params);
+    const response = await client.query(queryObject);
     await client.end();
     return response;
 }
