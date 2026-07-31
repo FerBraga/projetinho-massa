8e356ce724185332e337592922405b57c98a99a0
Fernando Braga <oferbraga108@gmail.com>
2026-07-31 09:42:04 -0300

    add jsconfig file to set base path and also change jestconfig file to include the base path.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 const nextJest = require("next/jest");
 
-const createJestConfig = nextJest();
-const jestConfig = createJestConfig({});
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
-module.exports = jestConfig;
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+});
+
+module.exports = module.exports = async () => {
+  const config = await jestConfig();
+  config.transformIgnorePatterns = ["node_modules/(?!.*)"];
+  return config;
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-import database from '../../../../../infra/database';
+import database from 'infra/database';
 
 test ('GET to /status should return status 200', async () => {
   const res = await fetch('http://localhost:3000/api/v1/status');
