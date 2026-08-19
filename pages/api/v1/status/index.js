a0de9e32bcd013bc20e94571f5cc2e1cfe7059ce
Fernando Braga <oferbraga108@gmail.com>
2026-08-19 09:58:06 -0300

    feat: add query parametrizada para os testes do endpoint do '/status'

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,27 @@
+import database from "infra/database.js";
 
 export default async function (req, res) {
+  //versão do postgress
+  const version = await database.query("SHOW server_version");
 
-  res.status(200).json({ status: 'ok', dbStatus: 'ok' });
+  const databaseName = process.env.DATABASE_NAME || "postgres";
+
+  //conexões máximas do postgress
+  const maxConnections = await database.query(
+    "SELECT setting FROM pg_settings WHERE name = 'max_connections'",
+  );
+  //conexões usadas do postgress
+  const openConnections = await database.query({
+    text: `SELECT * FROM pg_stat_activity WHERE datname = $1`,
+    values: [databaseName],
+  });
+
+  const updatedAt = new Date().toISOString();
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    version: version.rows[0].server_version,
+    max_connections: parseInt(maxConnections.rows[0].setting),
+    open_connections: parseInt(openConnections.rowCount),
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,18 @@
-import database from 'infra/database';
-
-test ('GET to /status should return status 200', async () => {
-  const res = await fetch('http://localhost:3000/api/v1/status');
-  console.log(database);
+test("GET to /status should return status 200", async () => {
+  const res = await fetch("http://localhost:3000/api/v1/status");
   const data = await res.json();
-  expect(data).toEqual({ status: 'ok', dbStatus: 'ok' });
+
+  expect(res.status).toBe(200);
+  expect(data).toHaveProperty("updated_at");
+  expect(data.updated_at).toBeDefined();
+  expect(data).toHaveProperty("version");
+  expect(data.version).toBeDefined();
+  expect(data.version).toEqual("15.18");
+  expect(data).toHaveProperty("max_connections");
+  expect(data.max_connections).toBeDefined();
+  expect(data.open_connections).toBeDefined();
+  expect(data.open_connections).toEqual(1);
+
+  const parsedDate = new Date(data.updated_at);
+  expect(parsedDate.toISOString()).toBe(data.updated_at);
 });
